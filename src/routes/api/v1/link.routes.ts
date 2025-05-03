7d7ff6ce2adc51823b5dd9fb1bfd0adf6fad4e5d
SMGhulam29091993 <smghulam.ghausfaiyaz19@gmail.com>
2025-05-03 23:38:33 +0530

    feat : added a route to fetch content via hash

--- src/routes/api/v1/link.routes.ts
+++ src/routes/api/v1/link.routes.ts
@@ -1,12 +1,13 @@
 import express from 'express';
 import { zodValidator } from '../../../middleware/zodValidate.middleware';
 import { createLinkSchema } from '../../../dto/link.dto';
-import { createBrainLink, createLink } from '../../../controller/link.controller';
+import { createBrainLink, createLink, getBrainLink } from '../../../controller/link.controller';
 import { AuthMiddleware } from '../../../middleware/auth.middleware';
 
 const routes = express.Router();
 
 routes.post('/create-link/:contentId', zodValidator(createLinkSchema), AuthMiddleware ,createLink);
 routes.post('/brain-link', zodValidator(createLinkSchema), AuthMiddleware ,createBrainLink);
+routes.get('/brain/:hash', getBrainLink);
 
 export default routes;
