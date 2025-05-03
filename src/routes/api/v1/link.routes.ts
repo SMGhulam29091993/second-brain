9372ac7787a73e557f1743010f14286e5124aca1
SMGhulam29091993 <smghulam.ghausfaiyaz19@gmail.com>
2025-05-03 23:36:42 +0530

    feat:  added a route the create brain link

--- src/routes/api/v1/link.routes.ts
+++ src/routes/api/v1/link.routes.ts
@@ -1,11 +1,12 @@
 import express from 'express';
 import { zodValidator } from '../../../middleware/zodValidate.middleware';
 import { createLinkSchema } from '../../../dto/link.dto';
-import { createLink } from '../../../controller/link.controller';
+import { createBrainLink, createLink } from '../../../controller/link.controller';
 import { AuthMiddleware } from '../../../middleware/auth.middleware';
 
 const routes = express.Router();
 
 routes.post('/create-link/:contentId', zodValidator(createLinkSchema), AuthMiddleware ,createLink);
+routes.post('/brain-link', zodValidator(createLinkSchema), AuthMiddleware ,createBrainLink);
 
 export default routes;
