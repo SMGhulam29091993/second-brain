ad1db2b7898051851ae94831fbf1d8de69eca5c3
SMGhulam29091993 <smghulam.ghausfaiyaz19@gmail.com>
2025-05-03 17:44:49 +0530

    feat : added routes to get all documentationa and delete content

--- src/routes/api/v1/content.routes.ts
+++ src/routes/api/v1/content.routes.ts
@@ -1,12 +1,14 @@
 import express from 'express';
 import { zodValidator } from '../../../middleware/zodValidate.middleware';
-import { addContent } from '../../../controller/content.controller';
+import { addContent, deleteContent, getAllContent } from '../../../controller/content.controller';
 import { AuthMiddleware } from '../../../middleware/auth.middleware';
 import { createContentSchema } from '../../../dto/content.dto';
 
 const routes = express.Router();
 
 routes.post('/add-content', zodValidator(createContentSchema), AuthMiddleware ,addContent);
+routes.get('/get-all-content', AuthMiddleware, getAllContent);
+routes.delete('/delete-content', AuthMiddleware, deleteContent);
 
 
 export default routes;
