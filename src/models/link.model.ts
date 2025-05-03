394ac0b926cd6db373355e05cc5e4dd7c1259f5f
SMGhulam29091993 <smghulam.ghausfaiyaz19@gmail.com>
2025-05-03 18:21:24 +0530

    feat : added model for link

---
+++ src/models/link.model.ts
@@ -0,0 +1,18 @@
+import mongoose from "mongoose";
+
+
+const linkSchema = new mongoose.Schema({
+    hash: {
+        type : String,
+        required : true,
+        unique : true,
+    },
+    userId : {
+        type : mongoose.Schema.Types.ObjectId,
+        ref : "User",
+        required : true,
+    }
+},{timestamps : true});
+
+const Link = mongoose.model("Link", linkSchema);
+export default Link;
