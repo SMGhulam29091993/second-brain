97e3e69ae812d88238d0e38cb98fb8ad3098ec39
SMGhulam29091993 <smghulam.ghausfaiyaz19@gmail.com>
2025-06-01 03:10:35 +0530

    fix: added a custom hashCode generatore for otp

--- src/controller/user.controller.ts
+++ src/controller/user.controller.ts
@@ -1,16 +1,16 @@
-import bcryptjs from 'bcryptjs';
-import { config } from 'dotenv';
+import bcryptjs from "bcryptjs";
+import { config } from "dotenv";
 import { NextFunction, Request, Response } from "express";
-import { UserDto } from '../constants/types';
-import { createUserInput } from '../dto/user.dto';
-import { generateVerifiactionCode, sendResponse } from '../lib/helper.function';
-import { sendMail } from '../lib/nodemailer';
-import Otp from '../models/otp.model';
-import User from '../models/user.model';
-import { createToken, validateToken } from '../utils/features';
+import { UserDto } from "../constants/types";
+import { createUserInput } from "../dto/user.dto";
+import { generateVerifiactionCode, sendResponse } from "../lib/helper.function";
+import { sendMail } from "../lib/nodemailer";
+import Otp from "../models/otp.model";
+import User from "../models/user.model";
+import { createToken, validateToken } from "../utils/features";
+import { generateHash } from "./link.controller";
 
 config();
-
 
 /**
  * Registers a new user in the system.
@@ -18,7 +18,7 @@ config();
  * @param req - The HTTP request object containing user registration details.
  * @param res - The HTTP response object used to send responses to the client.
  * @param next - The next middleware function in the Express.js request-response cycle.
- * 
+ *
  * @remarks
  * - Checks if a user with the provided email already exists. If so, responds with a 403 status.
  * - Hashes the user's password before saving it to the database.
@@ -29,67 +29,76 @@ config();
  * - Responds with a 201 status if the user is successfully created.
  * - If user creation fails, responds with a 400 status.
  * - Passes any errors to the next middleware function.
- * 
+ *
  * @throws Will pass any unexpected errors to the next middleware.
  */
-export const registerUser = async (req: Request, res : Response, next : NextFunction) : Promise<void> => {
-    try {
+export const registerUser = async (
+  req: Request,
+  res: Response,
+  next: NextFunction
+): Promise<void> => {
+  try {
+    let verificationHash: string | null = null;
 
-        let verificationHash: string | null = null;
-        
-        const existingUser = await User.findOne({ email: req.body.email });
-        if(existingUser) {
-            sendResponse(res, 403, false, "User already exists", existingUser);
-            return;
-        }
-
-        const hashPassword = await bcryptjs.hash(req.body.password, 10);
-        const userData : createUserInput = {
-            ...req.body,
-            password: hashPassword,
-        }
-        const registerUser = await User.create(userData);
-
-        const { password, ...user } = registerUser.toObject(); // Exclude password from the response
-        
-        if(!user.isEmailVerified){
-            verificationHash = await bcryptjs.hash(user.email, 10);
-        }
-        
-        if(!registerUser) {
-            sendResponse(res, 400, false, "User not created", null);
-            return;
-        }
-
-        let code = await generateVerifiactionCode(); // Generate a random verification code
-        const codeAndHashStore = await Otp.create({
-            userId: registerUser._id,
-            hashedCode: verificationHash,
-            code: code,
-        });
-        if(!codeAndHashStore) {
-            sendResponse(res, 400, false, "Code not created", null);
-        }
-        await sendMail(user.email, String(process.env.SMTP_USER), "Email Verification Code", `<h1>Welcome ${user.username}</h1><p>Please verify your email with the given code : ${code} "</p>`);
-        sendResponse(res, 201, true, "User created successfully", {hashedCode : verificationHash});
-        return;
-    } catch (error) {
-        next(error)
+    const existingUser = await User.findOne({ email: req.body.email });
+    if (existingUser) {
+      sendResponse(res, 403, false, "User already exists", existingUser);
+      return;
     }
-}
 
+    const hashPassword = await bcryptjs.hash(req.body.password, 10);
+    const userData: createUserInput = {
+      ...req.body,
+      password: hashPassword,
+    };
+    const registerUser = await User.create(userData);
+
+    const { password, ...user } = registerUser.toObject(); // Exclude password from the response
+
+    if (!user.isEmailVerified) {
+      verificationHash = await generateHash(10);
+    }
+
+    if (!registerUser) {
+      sendResponse(res, 400, false, "User not created", null);
+      return;
+    }
+
+    let code = await generateVerifiactionCode(); // Generate a random verification code
+    const codeAndHashStore = await Otp.create({
+      userId: registerUser._id,
+      hashedCode: verificationHash,
+      code: code,
+    });
+    if (!codeAndHashStore) {
+      sendResponse(res, 400, false, "Code not created", null);
+    }
+    await sendMail(
+      user.email,
+      String(process.env.SMTP_USER),
+      "Email Verification Code",
+      `<h1>Welcome ${user.username}</h1><p>Please verify your email with the given code : ${code} "</p>`
+    );
+    sendResponse(res, 201, true, "User created successfully", {
+      hashedCode: verificationHash,
+    });
+    return;
+  } catch (error) {
+    next(error);
+  }
+};
 
 /**
  * Handles the creation of a user session by authenticating the user and generating a token.
- * 
+ *
  * @param req - The HTTP request object containing user credentials in the body.
  * @param res - The HTTP response object used to send responses back to the client.
  * @param next - The next middleware function in the Express.js request-response cycle.
- * 
+ *
  * @returns A Promise that resolves to void. Sends appropriate HTTP responses based on the authentication process.
- * 
+ *
  * @throws Passes any unexpected errors to the next middleware for error handling.
- * 
+ *
  * Functionality:
  * - Fetches the user from the database using the provided email.
  * - Verifies the user's password using bcryptjs.
@@ -97,57 +106,71 @@ export const registerUser = async (req: Request, res : Response, next : NextFunc
  * - If authentication is successful, generates a token and sends it in the response.
  * - Excludes the password from the user object in the response.
  */
-export const createSession = async (req : Request, res : Response, next : NextFunction) : Promise<void> => {
-    try {
-        const fetchUser = await User.findOne({ email: req.body.email }).select("+password");
-        if(!fetchUser) {
-            sendResponse(res, 404, false, "User not found", null);
-            return;
-        }
-        const isValidPassword = await bcryptjs.compare(req.body.password, fetchUser.password);
-        if(!isValidPassword) {
-            sendResponse(res, 403, false, "Invalid Credentials", null);
-            return;
-        }
-
-        if(isValidPassword && !fetchUser.isEmailVerified) {
-            const existHashedCode = await Otp.findOne({ userId: fetchUser._id });
-            if(existHashedCode) {
-                await Otp.deleteOne({ userId: fetchUser._id });
-            }
-            const verificationHash = await bcryptjs.hash(fetchUser.email, 10);
-            const code = await generateVerifiactionCode(); // Generate a random verification code
-
-            await sendMail(fetchUser.email, String(process.env.SMTP_USER), "Email Verification Code", `<h1>Welcome ${fetchUser.username}</h1><p>Please verify your email with the given code : ${code} "</p>`);
-            sendResponse(res, 200, true, "User Email not verified", verificationHash);
-            return;
-        }
-        const { password, ...user } = fetchUser.toObject(); // Exclude password from the response
-
-        const userData : UserDto={
-            _id : user._id.toString(),
-            username : user.username,
-            email : user.email,
-            isEmailVerified : user.isEmailVerified
-        }
-        await createToken(res, userData, "Login successful");
-        return;
-    } catch (error) {
-        next(error)        
+export const createSession = async (
+  req: Request,
+  res: Response,
+  next: NextFunction
+): Promise<void> => {
+  try {
+    const fetchUser = await User.findOne({ email: req.body.email }).select(
+      "+password"
+    );
+    if (!fetchUser) {
+      sendResponse(res, 404, false, "User not found", null);
+      return;
     }
-}
+    const isValidPassword = await bcryptjs.compare(
+      req.body.password,
+      fetchUser.password
+    );
+    if (!isValidPassword) {
+      sendResponse(res, 403, false, "Invalid Credentials", null);
+      return;
+    }
+
+    if (isValidPassword && !fetchUser.isEmailVerified) {
+      const existHashedCode = await Otp.findOne({ userId: fetchUser._id });
+      if (existHashedCode) {
+        await Otp.deleteOne({ userId: fetchUser._id });
+      }
+      const verificationHash = await bcryptjs.hash(fetchUser.email, 10);
+      const code = await generateVerifiactionCode(); // Generate a random verification code
+
+      await sendMail(
+        fetchUser.email,
+        String(process.env.SMTP_USER),
+        "Email Verification Code",
+        `<h1>Welcome ${fetchUser.username}</h1><p>Please verify your email with the given code : ${code} "</p>`
+      );
+      sendResponse(res, 200, true, "User Email not verified", verificationHash);
+      return;
+    }
+    const { password, ...user } = fetchUser.toObject(); // Exclude password from the response
+
+    const userData: UserDto = {
+      _id: user._id.toString(),
+      username: user.username,
+      email: user.email,
+      isEmailVerified: user.isEmailVerified,
+    };
+    await createToken(res, userData, "Login successful");
+    return;
+  } catch (error) {
+    next(error);
+  }
+};
 
 /**
  * Handles the refresh token functionality for user authentication.
- * 
+ *
  * @param req - The HTTP request object, which contains the cookies with the refresh token.
  * @param res - The HTTP response object, used to send the response back to the client.
  * @param next - The next middleware function in the Express.js request-response cycle.
- * 
+ *
  * @returns A Promise that resolves to void. Sends a response to the client with the refreshed token or an error message.
- * 
+ *
  * @throws Passes any unexpected errors to the next middleware for error handling.
- * 
+ *
  * The function performs the following steps:
  * 1. Extracts the refresh token from the request cookies.
  * 2. Validates the token and retrieves the associated user data.
@@ -156,42 +179,46 @@ export const createSession = async (req : Request, res : Response, next : NextFu
  * 5. Creates a new token and sends it back to the client.
  * 6. Handles errors by passing them to the next middleware.
  */
-export const refreshToken = async (req: Request, res : Response, next : NextFunction) : Promise<void>=>{
-    try {
-        const token = req.cookies['refresh-token'];
-        if(!token) {
-            sendResponse(res, 403, false, "Unauthorized Action", null);
-            return;
-        }
-        const data = await validateToken(token);
-        const userData = await User.findById({_id : data})
-        if(!userData) {
-            sendResponse(res, 403, false, "Unauthorized Action", null);
-            return;
-        }
-        const user : UserDto = {
-            _id : userData._id.toString(),
-            username : userData.username,
-            email : userData.email,
-            isEmailVerified : userData.isEmailVerified
-        }
-        await createToken(res, user, "Token refreshed successfully");
-        return;
-    } catch (error) {
-        next(error);
+export const refreshToken = async (
+  req: Request,
+  res: Response,
+  next: NextFunction
+): Promise<void> => {
+  try {
+    const token = req.cookies["refresh-token"];
+    if (!token) {
+      sendResponse(res, 403, false, "Unauthorized Action", null);
+      return;
     }
-}
+    const data = await validateToken(token);
+    const userData = await User.findById({ _id: data });
+    if (!userData) {
+      sendResponse(res, 403, false, "Unauthorized Action", null);
+      return;
+    }
+    const user: UserDto = {
+      _id: userData._id.toString(),
+      username: userData.username,
+      email: userData.email,
+      isEmailVerified: userData.isEmailVerified,
+    };
+    await createToken(res, user, "Token refreshed successfully");
+    return;
+  } catch (error) {
+    next(error);
+  }
+};
 
 /**
  * Verifies the user's email by validating the provided OTP code.
- * 
- * @param req - The HTTP request object containing the hashed OTP code in `params` 
+ *
+ * @param req - The HTTP request object containing the hashed OTP code in `params`
  *              and the plain OTP code in the request body.
  * @param res - The HTTP response object used to send the response back to the client.
  * @param next - The next middleware function in the Express.js request-response cycle.
- * 
+ *
  * @throws Will pass any unexpected errors to the next middleware.
- * 
+ *
  * The function performs the following steps:
  * 1. Retrieves the hashed OTP code from the request parameters and the plain OTP code from the request body.
  * 2. Searches for the OTP data in the database using the hashed code.
@@ -204,44 +231,46 @@ export const refreshToken = async (req: Request, res : Response, next : NextFunc
  * 9. Deletes the OTP data associated with the user.
  * 10. Creates a token for the user and sends a success response with the token.
  */
-export const verifyEmail = async (req : Request, res : Response, next : NextFunction) : Promise<void> => {
-    try {
-        const hashedCode = req.params.hashedCode;
-        const code = req.body.code;
-        const otpData = await Otp.findOne({ hashedCode: hashedCode });
-        if(!otpData) {
-            sendResponse(res, 403, false, "Invalid code", null);
-            return;
-        }
-        const fetchUser = await User.findById(otpData.userId);
-        if(!fetchUser) {
-            sendResponse(res, 403, false, "Invalid code", null);
-            return;
-        }
-        if(otpData.code !== code) {
-            sendResponse(res, 403, false, "Invalid code", null);
-            return;
-        }
-        fetchUser.isEmailVerified = true;
-        await fetchUser.save();
-
-        await Otp.deleteOne({ userId: fetchUser._id });
-
-
-        let userTokenData : UserDto = {
-            _id : fetchUser._id.toString(),
-            username : fetchUser.username,
-            email : fetchUser.email,
-            isEmailVerified : fetchUser.isEmailVerified
-        }
-        await createToken(res, userTokenData, "Email verified successfully");
-        return;        
-        
-    } catch (error) {
-        console.error(error);
-        next(error);
+export const verifyEmail = async (
+  req: Request,
+  res: Response,
+  next: NextFunction
+): Promise<void> => {
+  try {
+    const hashedCode = req.params.hashedCode;
+    const code = req.body.code;
+    const otpData = await Otp.findOne({ hashedCode: hashedCode });
+    if (!otpData) {
+      sendResponse(res, 403, false, "Invalid code", null);
+      return;
     }
-}
+    const fetchUser = await User.findById(otpData.userId);
+    if (!fetchUser) {
+      sendResponse(res, 403, false, "Invalid code", null);
+      return;
+    }
+    if (otpData.code !== code) {
+      sendResponse(res, 403, false, "Invalid code", null);
+      return;
+    }
+    fetchUser.isEmailVerified = true;
+    await fetchUser.save();
+
+    await Otp.deleteOne({ userId: fetchUser._id });
+
+    let userTokenData: UserDto = {
+      _id: fetchUser._id.toString(),
+      username: fetchUser.username,
+      email: fetchUser.email,
+      isEmailVerified: fetchUser.isEmailVerified,
+    };
+    await createToken(res, userTokenData, "Email verified successfully");
+    return;
+  } catch (error) {
+    console.error(error);
+    next(error);
+  }
+};
 
 /**
  * Handles the destruction of a user session by clearing the "refresh-token" cookie.
@@ -259,15 +288,27 @@ export const verifyEmail = async (req : Request, res : Response, next : NextFunc
  * - Sends a response with a success message upon successful logout.
  * - Passes any errors to the next middleware using the `next` function.
  */
-export const destroySession = async (req : Request, res : Response, next : NextFunction) : Promise<void> => {
-    try {
-        res.status(200).clearCookie("refresh-token",{
-            httpOnly: true,
-            secure: process.env.NODE_ENV === 'production'? true : false,
-            sameSite: process.env.NODE_ENV === 'production' ? 'strict' : 'lax'
-        }).send({statusCode : 200, success: true, message : "Logout successful", data : null});
-        return;
-    } catch (error) {
-        next(error);
-    }
-}
+export const destroySession = async (
+  req: Request,
+  res: Response,
+  next: NextFunction
+): Promise<void> => {
+  try {
+    res
+      .status(200)
+      .clearCookie("refresh-token", {
+        httpOnly: true,
+        secure: process.env.NODE_ENV === "production" ? true : false,
+        sameSite: process.env.NODE_ENV === "production" ? "strict" : "lax",
+      })
+      .send({
+        statusCode: 200,
+        success: true,
+        message: "Logout successful",
+        data: null,
+      });
+    return;
+  } catch (error) {
+    next(error);
+  }
+};
