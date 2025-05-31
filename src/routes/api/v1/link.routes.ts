5064d7c2170d7ec5308da5c199f8b9d7ce9404ca
SMGhulam29091993 <smghulam.ghausfaiyaz19@gmail.com>
2025-05-31 15:29:30 +0530

    fix: updated the create braing link routes

--- src/routes/api/v1/link.routes.ts
+++ src/routes/api/v1/link.routes.ts
@@ -1,13 +1,22 @@
-import express from 'express';
-import { zodValidator } from '../../../middleware/zodValidate.middleware';
-import { createLinkSchema } from '../../../dto/link.dto';
-import { createBrainLink, createLink, getBrainLink } from '../../../controller/link.controller';
-import { AuthMiddleware } from '../../../middleware/auth.middleware';
+import express from "express";
+import { zodValidator } from "../../../middleware/zodValidate.middleware";
+import { createLinkSchema } from "../../../dto/link.dto";
+import {
+  createBrainLink,
+  createLink,
+  getBrainLink,
+} from "../../../controller/link.controller";
+import { AuthMiddleware } from "../../../middleware/auth.middleware";
 
 const routes = express.Router();
 
-routes.post('/create-link/:contentId', zodValidator(createLinkSchema), AuthMiddleware ,createLink);
-routes.post('/brain-link', zodValidator(createLinkSchema), AuthMiddleware ,createBrainLink);
-routes.get('/brain/:hash', getBrainLink);
+routes.post(
+  "/create-link/:contentId",
+  zodValidator(createLinkSchema),
+  AuthMiddleware,
+  createLink
+);
+routes.post("/brain-link", AuthMiddleware, createBrainLink);
+routes.get("/brain/:hash", getBrainLink);
 
 export default routes;
