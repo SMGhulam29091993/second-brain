5ad02083fac24b68de1856119b1f52f4936e2244
SMGhulam29091993 <smghulam.ghausfaiyaz19@gmail.com>
2025-06-28 17:41:30 +0530

    feat: added the route for reset and forgot password

--- src/routes/api/v1/user.routes.ts
+++ src/routes/api/v1/user.routes.ts
@@ -2,17 +2,29 @@ import express from "express";
 import {
   createSession,
   destroySession,
+  forgotPassword,
   refreshToken,
   registerUser,
+  resetPassword,
   verifyEmail,
 } from "../../../controller/user.controller";
-import { createUserSchema, userLoginSchema } from "../../../dto/user.dto";
+import {
+  createUserSchema,
+  resetPasswordSchema,
+  userLoginSchema,
+} from "../../../dto/user.dto";
 import { zodValidator } from "../../../middleware/zodValidate.middleware";
 
 const routes = express.Router();
 
 routes.post("/register", zodValidator(createUserSchema), registerUser);
 routes.post("/login", zodValidator(userLoginSchema), createSession);
+routes.post(
+  "/send-reset-password-email",
+  zodValidator(resetPasswordSchema),
+  forgotPassword
+);
+routes.put("/reset-password/:userId", resetPassword);
 routes.post("/verify/:hashedCode", verifyEmail);
 routes.get("/refresh-token", refreshToken);
 routes.post("/logout", destroySession);
