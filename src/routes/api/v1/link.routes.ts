4d9ea57eb227fd8de8f76a8ded05757c0ee1072b
SMGhulam29091993 <smghulam.ghausfaiyaz19@gmail.com>
2025-06-30 02:15:20 +0530

    feat: added route to post and get summary

--- src/routes/api/v1/link.routes.ts
+++ src/routes/api/v1/link.routes.ts
@@ -4,7 +4,9 @@ import { createLinkSchema } from "../../../dto/link.dto";
 import {
   createBrainLink,
   createLink,
+  createSummaryLink,
   getBrainLink,
+  getSummaryLink,
 } from "../../../controller/link.controller";
 import { AuthMiddleware } from "../../../middleware/auth.middleware";
 
@@ -18,5 +20,7 @@ routes.post(
 );
 routes.post("/brain-link", AuthMiddleware, createBrainLink);
 routes.get("/brain/:hash", getBrainLink);
+routes.post("/summary-link/:contentId", AuthMiddleware, createSummaryLink);
+routes.get("/summary/:hash", getSummaryLink);
 
 export default routes;
